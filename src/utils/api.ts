0f5d22c855bf4d92950b5a4f0472d34b392f726d
Elizaveta Veselova <veselovae1605@yandex.ru>
2024-12-03 12:30:54 +0300

    ДЗ5. axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,25 @@
+import axios from "axios";
 import { IProductItem } from "@src/store/model/interfaces";
 
 const API_URL = "http://localhost:3006";
 
 export const getCatalogItem = async (id: string): Promise<IProductItem> => {
-  const response = await fetch(`${API_URL}/item/${id}`);
-  const { content } = await response.json();
-  return content;
+  return await axios
+    .get(`${API_URL}/item/${id}`)
+    .then(({ data: { content } }) => content);
 };
 
 export const getCatalogItems = async (): Promise<IProductItem[]> => {
-  const response = await fetch(`${API_URL}/item`);
-  const { content } = await response.json();
-  return content;
+  return await axios
+    .get(`${API_URL}/item`)
+    .then(({ data: { content } }) => content);
 };
 
 export const getItemPhoto = async (path: string): Promise<string> => {
-  const response = await fetch(`${API_URL}/${path}`);
-  const blob = await response.blob();
-  const newBlob = new Blob([blob]);
-  return window.URL.createObjectURL(newBlob);
+  const blob = await axios
+    .get(`${API_URL}/${path}`, {
+      responseType: "blob",
+    })
+    .then(({ data }): Blob => new Blob([data]));
+  return window.URL.createObjectURL(blob);
 };
