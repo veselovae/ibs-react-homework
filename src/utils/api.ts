90ee8b70f64371226c2a33189d5413a63ec039d7
Elizaveta Veselova <veselovae1605@yandex.ru>
2024-12-03 15:46:29 +0300

    ДЗ5. Добавлены типы, функция обработки результатов ответа

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,18 +1,26 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { IProductItem } from "@src/store/model/interfaces";
+import { IDataCatalogItem, IDataCatalogItems } from "./model/interfaces";
 
 const API_URL = "http://localhost:3006";
 
 export const getCatalogItem = async (id: string): Promise<IProductItem> => {
   return await axios
     .get(`${API_URL}/item/${id}`)
-    .then(({ data: { content } }) => content);
+    .then(
+      ({ data: { content } }: AxiosResponse<IDataCatalogItem>): IProductItem =>
+        content,
+    );
 };
 
 export const getCatalogItems = async (): Promise<IProductItem[]> => {
   return await axios
     .get(`${API_URL}/item`)
-    .then(({ data: { content } }) => content);
+    .then(
+      ({
+        data: { content },
+      }: AxiosResponse<IDataCatalogItems>): IProductItem[] => content,
+    );
 };
 
 export const getItemPhoto = async (path: string): Promise<string> => {
@@ -20,6 +28,6 @@ export const getItemPhoto = async (path: string): Promise<string> => {
     .get(`${API_URL}/${path}`, {
       responseType: "blob",
     })
-    .then(({ data }): Blob => new Blob([data]));
+    .then(({ data }: AxiosResponse<Blob>): Blob => data);
   return window.URL.createObjectURL(blob);
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { IProductItem } from "@src/store/model/interfaces";
+
 export const debounce = <T>(callback: Function, delay: number) => {
   let timeoutId: ReturnType<typeof setTimeout>;
   return function (...args: T[]) {
@@ -8,5 +10,18 @@ export const debounce = <T>(callback: Function, delay: number) => {
     timeoutId = setTimeout(() => {
       callback(...args);
     }, delay);
+  };
+};
+
+export const processResponse = (response: IProductItem): IProductItem => {
+  return {
+    ...response,
+    name: response?.name ?? "Название",
+    description: response?.description ?? "",
+    details: response?.details ?? "",
+    price: {
+      ...response.price,
+      value: response?.price?.value ?? "",
+    },
   };
 };
